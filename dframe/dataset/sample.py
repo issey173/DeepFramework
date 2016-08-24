d8a98d90f09814ff020eb07828070a3b156992c4
Issey Masuda Mora <issey183@outlook.com>
2016-08-24 17:59:30 +0200

    Distribution files added to gitignore and correct implementation of ABCMeta for Input and Ouput

--- dframe/dataset/sample.py
+++ dframe/dataset/sample.py
@@ -1,21 +1,19 @@
 from abc import ABCMeta, abstractmethod
 
 
-class Input(metaclass=ABCMeta):
+# noinspection PyClassHasNoInit
+class Input:
     """Interface like class for all the classes that are meant to be an input"""
-
-    def __init__(self):
-        pass
+    __metaclass__ = ABCMeta
 
     @abstractmethod
     def get_input(self): pass
 
 
-class Output(metaclass=ABCMeta):
+# noinspection PyClassHasNoInit
+class Output:
     """Interface like class for all the classes that are meant to be an output"""
-
-    def __init__(self):
-        pass
+    __metaclass__ = ABCMeta
 
     @abstractmethod
     def get_output(self): pass
