1f139f7879c8a277da222a6bbed2388edee51e1c
Issey Masuda Mora <issey173@gmail.com>
2016-10-11 19:55:22 +0200

    Requirements & setup.py updated

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-from setuptools import setup
+import setuptools
 
 import dframe
 
-setup(
+
+def get_dependencies():
+    with open('requirements.txt', 'r') as f:
+        dependencies = list(f)
+        dependencies = [dependency.replace('\n', '') for dependency in dependencies]
+        return dependencies
+
+
+setuptools.setup(
     name='DeepFramework',
     version=dframe.__version__,
     description='Python framework for Deep Learning',
     url='https://github.com/issey173/deepframework.git',
     author='Issey Masuda Mora',
-    author_email='issey183@outlook.com',
+    author_email='issey173@gmail.com',
     license='MIT',
     packages=['dframe'],
-    install_requires=[],
+    install_requires=get_dependencies(),
     zip_safe=False
 )
