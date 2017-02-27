d2caa21264ec23b623d8f15cf97b552ddc37e197
Issey Masuda Mora <issey173@gmail.com>
2017-02-27 16:38:30 +0100

    Build method to the Model class

--- dframe/model/model.py
+++ dframe/model/model.py
@@ -8,17 +8,28 @@ class Model:
     __metaclass__ = ABCMeta
 
     @abstractmethod
+    def build(self):
+        """Creates the architecture of the model given its parameters"""
+        pass
+
+    @abstractmethod
     def train(self, dataset):
+        """Trains the model with the given dataset"""
         pass
 
     @abstractmethod
     def validate(self, dataset):
+        """Validates the model using the given dataset"""
         pass
 
     @abstractmethod
     def test(self, dataset):
+        """Tests the model using the given dataset.
+
+        Returns the collection of results, one for each sample"""
         pass
 
     @abstractmethod
     def predict(self, sample):
+        """Predicts the output for the given sample"""
         pass
